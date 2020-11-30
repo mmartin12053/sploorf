dd1de61af3636c3d0de2e3d6bd760039ccc52e3d
Martin_Maximus <Martin_Maximus@student.mahoningctc.com>
2020-11-30 11:20:44 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// alert('sploorf')
+
+let x = 5;
+let y = 10;
+let xSuared = Math.pow(x, 2);
+let ySuared = Math.pow(y, 2);
+let z = Math.sqrt(xSuared+ySuared);
+let name = 'sploorf';
+console.log('your name is ' + name);
+console.log('x + y = ' + x+y);
+console.log('x + y = ' + parseInt(x+y));
+
+
+alert('hello, what is your name?');
+name = prompt('please input a name:');
+alert(`welcome ${name}. let him in.`);
+console.log(`your name is now ${name}`);
