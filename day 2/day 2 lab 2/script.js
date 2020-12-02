478260659d356123f949cc4de6ad71f369722ba5
Martin_Maximus <Martin_Maximus@student.mahoningctc.com>
2020-12-02 12:37:29 -0500

    Ninja2

---
+++ day 2/day 2 lab 2/script.js
@@ -0,0 +1,27 @@
+var apples = 10;
+var ninjas = 100;
+
+document.write('you have ' + apples + ' apple.<br>')
+apples++
+document.write('you have ' + apples + ' apple.<br>')
+apples++
+document.write('you have ' + apples + ' apple.<br>')
+apples++
+document.write('you have ' + apples + ' apple.<br>')
+apples++
+document.write('you have ' + apples + ' apple.<br>')
+apples++
+document.write('you have ' + apples + ' apple.<br> <br><br><br>')
+
+document.write('there are ' + ninjas + ' ninjas in the room.<br>')
+ninjas--
+document.write('there are ' + ninjas + ' ninjas in the room.<br>')
+ninjas--
+document.write('there are ' + ninjas + ' ninjas in the room.<br>')
+ninjas--
+document.write('there are ' + ninjas + ' ninjas in the room.<br>')
+ninjas--
+document.write('there are ' + ninjas + ' ninjas in the room.<br>')
+ninjas--
+document.write('there are ' + ninjas + ' ninjas in the room.<br>')
+ninjas--
